0782ed6056607ef76094c3f6d6d92f828e07b4a9
Donika <marchevadonika@gmail.com>
2023-11-15 22:59:07 +0200

    add service method for games

---
+++ 07.workshop-routing/client/src/services/gameService.js
@@ -0,0 +1,24 @@
+import { request } from "../lib/request.js";
+
+
+const baseUrl = 'http://localhost:3030/jsonstore/games'
+
+export const getAll = async () => {
+    const result= await request('GET', baseUrl);
+
+    return Object.values(result);
+}
+
+export const create = async (gameData) => {
+    const response = fetch(`${baseUrl}`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(gameData)
+    });
+    const result = await response.json();
+
+    return result;
+}
+
