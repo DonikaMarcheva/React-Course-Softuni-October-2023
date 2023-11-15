ee1f92516cb64d111294b4372a3cc18ee70631d2
Donika <marchevadonika@gmail.com>
2023-11-15 15:03:13 +0200

    add home component

--- 07.workshop-routing/client/src/App.jsx
+++ 07.workshop-routing/client/src/App.jsx
@@ -1,4 +1,5 @@
-import { Header } from "./components/Header/Header.jsx"
+import { Header } from "./components/header/Header.jsx"
+import Home from "./components/home/Home.jsx"
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div id="box">
      <Header/>
+     <Home/>
     </div>
   )
 }
