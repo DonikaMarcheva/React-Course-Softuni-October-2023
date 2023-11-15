38319d526ac6f57cd199aa24071e1cfe9cf2a88e
Donika <marchevadonika@gmail.com>
2023-11-15 15:21:04 +0200

    add games component

--- 07.workshop-routing/client/src/App.jsx
+++ 07.workshop-routing/client/src/App.jsx
@@ -1,13 +1,19 @@
-import { Header } from "./components/header/Header.jsx"
+import GameList from "./components/game-list/GameList.jsx"
+import Header from "./components/header/Header.jsx"
 import Home from "./components/home/Home.jsx"
+import { Routes, Route } from 'react-router-dom'
 
 
 function App() {
 
   return (
     <div id="box">
-     <Header/>
-     <Home/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<GameList />} />
+
+      </Routes>
     </div>
   )
 }
