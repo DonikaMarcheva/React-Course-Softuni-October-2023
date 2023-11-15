6195e2257fb36d9fd8fd380d7aca1234c9e0e1ca
Donika <marchevadonika@gmail.com>
2023-11-15 16:43:08 +0200

    add login component

--- 07.workshop-routing/client/src/App.jsx
+++ 07.workshop-routing/client/src/App.jsx
@@ -1,7 +1,9 @@
+import GameCreate from "./components/game-create/gameCreate.jsx"
 import GameList from "./components/game-list/GameList.jsx"
 import Header from "./components/header/Header.jsx"
 import Home from "./components/home/Home.jsx"
 import { Routes, Route } from 'react-router-dom'
+import Login from "./components/login/Login.jsx"
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/games" element={<GameList />} />
+        <Route path="/games/create" element={<GameCreate />} />
+        <Route path="/login" element={<Login />} />
 
       </Routes>
     </div>
