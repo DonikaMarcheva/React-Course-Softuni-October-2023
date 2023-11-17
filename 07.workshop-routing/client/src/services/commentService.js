84cde43f1e98285f33dd2536b6cbdf075d213380
Donika <marchevadonika@gmail.com>
2023-11-17 23:06:13 +0200

    update gameService

--- 07.workshop-routing/client/src/services/commentService.js
+++ 07.workshop-routing/client/src/services/commentService.js
@@ -2,11 +2,17 @@ import * as request from "../lib/request.js"
 
 const baseUrl = 'http://localhost:3030/jsonstore/comments';
 
-export const getAll =async () =>{
-    const result = await request.get(baseUrl);
 
-    return Object.values(result);
-}
+export const getAll = async (gameId) => {
+    const query = new URLSearchParams({
+        where: `gameId="${gameId}"`
+    });
+
+    const result = await request.get(`${baseUrl}`);
+
+    // TODO: temp solution until migration to collections service 
+    return Object.values(result).filter(comment => comment.gameId === gameId);
+};
 
 export const create = async (gameId, userName, text) => {
     const newComment = await request.post(baseUrl,{
